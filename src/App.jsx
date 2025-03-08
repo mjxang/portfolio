b6ac0e5c322b58bfbfb515d835952ad43fcbb2f1
mjxang <mjxang@gmail.com>
2025-03-07 22:15:39 -0500

    Fixed routing for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import {Home, About, Contact, Projects} from './pages/index'
 const App = () => {
   return (
     <main className="bg-slate-300/20 h-full">
-      <Router>
+      <Router basename="/portfolio">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,13 +8,13 @@ const Navbar = () => {
         <p className="blue-gradient_text">MJ</p>
       </NavLink>
       <nav className="flex text-lg gap-7 font-medium">
-        <NavLink to="/about" className={({ isActive }) => isActive ? "text-blue-500" : "text-black"}>
+        <NavLink to="/about" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
           About
         </NavLink>
-        <NavLink to="/projects" className={({ isActive }) => isActive ? "text-blue-500" : "text-black"}>
+        <NavLink to="/projects" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
           Projects
         </NavLink>
-        <NavLink to="/contact" className={({ isActive }) => isActive ? "text-blue-500" : "text-black"}>
+        <NavLink to="/contact" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
           Contact
         </NavLink>
       </nav>
